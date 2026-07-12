4ed536c209fc8046fe44708b79ab424692903ddc
honghoaibao <honghoaibaoat@gmail.com>
2026-07-12 03:03:05 +0000

    ok

--- app/src/main/java/com/gamespace/protection/ProtectedAppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.gamespace.protection
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [ProtectedAppEntity::class], version = 1, exportSchema = false)
-abstract class ProtectedAppDatabase : RoomDatabase() {
-    abstract fun protectedAppDao(): ProtectedAppDao
-}
